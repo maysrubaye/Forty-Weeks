843b90866e5f1d36ef5a3d0176bf9f0103f025ab
mays <mrubaye@terpmail.umd.edu>
2020-06-22 00:24:16 -0400

    new front page style and add featured poem

--- home/models.py
+++ home/models.py
@@ -7,7 +7,8 @@ from wagtail.core.fields import RichTextField
 from wagtail.core.fields import StreamField
 from wagtail.core import blocks
 from wagtail.images.blocks import ImageChooserBlock
-from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel, PageChooserPanel
+from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.contrib.table_block.blocks import TableBlock
 from wagtail.core import hooks
 from poemsite import custom_blocks
@@ -16,12 +17,20 @@ from wagtail.search import index
 class HomePage(Page):
     main_text = RichTextField(blank=True)
     sub_text = RichTextField(blank=True)
-    scroll_text = RichTextField(blank=True)
+    button_text = RichTextField(blank=True)
+    title_above_the_collection = RichTextField(blank=True)
+    home_page_image = models.ForeignKey(
+        'wagtailimages.Image', null=True, blank=True, 
+        on_delete=models.SET_NULL, related_name='+', verbose_name='Home Page Image')
+    featured_poem = models.ForeignKey('wagtailcore.Page', null=True, related_name='+', on_delete=models.SET_NULL)
 
     content_panels = Page.content_panels + [
         FieldPanel('main_text', blocks.RichTextBlock(required=False)),
         FieldPanel('sub_text', blocks.RichTextBlock(required=False)),
-        FieldPanel('scroll_text', blocks.RichTextBlock(required=False))
+        FieldPanel('button_text', blocks.RichTextBlock(required=False)),
+        FieldPanel('title_above_the_collection', blocks.RichTextBlock(required=False)),
+        ImageChooserPanel('home_page_image'),
+        PageChooserPanel('featured_poem'),
     ]
     
 class About(Page):
